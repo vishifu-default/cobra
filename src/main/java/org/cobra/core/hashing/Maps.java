bf9f69fe2ed653b182ad7a57e7fef8bc2ea0f90e
shifuvn <hungntsol77@gmail.com>
2024-11-09 15:23:43 +0700

    [core] hashing LookupTable
    - implements a key-value pair table for store hashing-pointer

--- src/main/java/org/cobra/core/hashing/Maps.java
+++ src/main/java/org/cobra/core/hashing/Maps.java
@@ -1,23 +1,23 @@
 package org.cobra.core.hashing;
 
-public interface Indexable<V> {
+public interface Maps {
 
     /**
      * Puts a key-value into index
      *
      * @return hash index of key
      */
-    int put(String key, V value);
+    int put(String key, byte[] value);
 
     /**
      * Gets value of record by key
      */
-    V get(String key);
+    byte[] get(String key);
 
     /**
      * Removes a key-value record from index
      */
-    V remove(String key);
+    byte[] remove(String key);
 
     /**
      * Test as if key is existed in index
